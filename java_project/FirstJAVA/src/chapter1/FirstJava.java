b8d04074027b79fedacb0e4f7e74961b6260861d
지용욱 <jyu7650@gmail.com>
2020-04-08 18:16:14 +0900

    Update FirstJava.java

--- java_project/FirstJAVA/src/chapter1/FirstJava.java
+++ java_project/FirstJAVA/src/chapter1/FirstJava.java
@@ -4,12 +4,14 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num;
+		num = 10 + 20;
 		
 		
 			
-			System.out.println("Hellow~!! JAVA~!!");
-			System.out.println("문자열(string)" + "Hellow~!! JAVA~!!");
-			System.out.println(1+1);
+		System.out.println("Hellow~!! JAVA~!!");
+		System.out.println("문자열(string)" + "Hellow~!! JAVA~!!");
+		System.out.println(num);
 			
 			
 			
