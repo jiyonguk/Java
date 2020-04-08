bf4d488b729747b7cc5340795df935dd3ea81a3b
지용욱 <jyu7650@gmail.com>
2020-04-08 17:46:25 +0900

    Create FirstJava.java

---
+++ java_project/FirstJAVA/src/chapter1/FirstJava.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+			
+			System.out.println("Hellow~!! JAVA~!!");
+			System.out.println("542452452" + "Hellow~!! JAVA~!!");
+	}
+
+}
