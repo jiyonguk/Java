b9810bfe832d5554ce5ce2058ca7b1dbe636e5ba
지용욱 <jyu7650@gmail.com>
2020-04-16 19:49:50 +0900

    20200416

---
+++ java_project/Cp6-ClassAndInstance/src/ex/Ex3_4.java
@@ -0,0 +1,13 @@
+package ex;
+
+public class Ex3_4 {
+	
+	boolean dec; 
+	
+	
+	void Dec(int num) {
+		if(num==2||num%2==1) {
+			dec = !dec;
+		}
+	}
+}
---
+++ java_project/Cp6-ClassAndInstance/src/ex/Ex3_4M.java
@@ -0,0 +1,15 @@
+package ex;
+
+public class Ex3_4M {
+
+	public static void main(String[] args) {
+		Ex3_4 dec = new Ex3_4();
+		
+		
+		dec.Dec(4);
+		System.out.println(dec.dec);
+		
+
+	}
+
+}
