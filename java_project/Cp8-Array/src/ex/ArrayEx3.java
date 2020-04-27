68b1ef58219a171555d3d22b50a69b541228014a
지용욱 <jyu7650@gmail.com>
2020-04-27 19:13:13 +0900

    20200427

--- java_project/Cp8-Array/src/ex/ArrayEx3.java
+++ java_project/Cp8-Array/src/ex/ArrayEx3.java
@@ -3,8 +3,19 @@ package ex;
 public class ArrayEx3 {
 
 
-	public static void main(String[] args) {
-		int[]arr = new int[ar[].length];
-		ar
+	public static void changeArr(int[][] arr) {
+		
+		
+		
+		int[]temp = new int[arr[0].length];
+		
+		temp = arr[arr.length-1];
+		
+		for(int i=arr.length-1; i>0; i--) {
+			arr[i] = arr[i-1];
+		}
+		
+		arr[0]=temp;
+		
 	}
 }
