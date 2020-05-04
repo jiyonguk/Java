9113db52d210d46e5bd61e7a13e8ea14a441b987
지용욱 <jyu7650@gmail.com>
2020-05-04 18:18:02 +0900

    20200504

--- java_project/JJJProject/src/ex/dungeon.java
+++ java_project/JJJProject/src/ex/dungeon.java
@@ -3,37 +3,75 @@ package ex;
 import java.util.Scanner;
 
 public class dungeon {
-	
-	int stage;
+
+	boolean result;
 	Scanner sc;
-	
-	dungeon(int num){
-		this.stage = num;
+
+	dungeon() {
+		this.result = false;
 		sc = new Scanner(System.in);
 	}
-	
+
 	void stage() {
+
+		// battle();
+
 		
-		 
-		
-		//battle();
-		
-		stage = 0;
 	}
-	
-	void stageChoice(){
-		
-		System.out.println("스테이지를 선택해주세요.");
-		int num = sc.nextInt();
-		sc.nextLine();
-		
-		switch(num) {
-		
-		case 1:
-		}
-	
+
+	void stageChoice() {
+
+		while (true) {
+			System.out.println("스테이지를 선택해주세요.");
+			System.out.println("1.EASY 2.NOMAL 3.HARD");
+			int num = sc.nextInt();
+			sc.nextLine();
+
+			switch (num) {
+
+			case dungeonIf.EASY:
+				EasyStage();
+				if (result) {
+					System.out.println("실패하셨습니다.");
+					continue;
+				}
+			case dungeonIf.NOMAL:
+				NomalStage();
+				if (result) {
+					System.out.println("실패하셨습니다.");
+					continue;
+				}
+			case dungeonIf.HARD:
+				HardStage();
+				if (result) {
+					System.out.println("실패하셨습니다.");
+					continue;
+				}
+			}
+		} // 스테이지 선택 반복
+
 	}
-	
-	
-	boolean revelCheck(){return false;}
+
+	boolean EasyStage() {
+		result=false;
+		// rat, chicken, rabbit, boss dog
+		return result;
+	}
+
+	boolean NomalStage() {
+		result=false;
+		// monkey, sheep, pig, boss snake
+		return result;
+	}
+
+	boolean HardStage() {
+		result=false;
+		// horse, cow, tiger, boss dragon
+		return result;
+	}
+
+	boolean revelCheck() {
+		return false;
+	}
+
 }
---
+++ java_project/JJJProject/src/ex/dungeonIf.java
@@ -0,0 +1,7 @@
+package ex;
+
+public interface dungeonIf {
+	int EASY = 1;
+	int NOMAL = 2;
+	int HARD = 3;
+}
