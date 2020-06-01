6c2feb562c84c39a4c20be00ef7593ee2c24004f
지용욱 <jyu7650@gmail.com>
2020-06-01 15:01:32 +0900

    jdbc과제

---
+++ java_project/Cp17-JDBC/src/manager/MangerMain.java
@@ -0,0 +1,30 @@
+package manager;
+
+import java.util.Scanner;
+
+public class MangerMain {
+
+	public static void main(String[] args) {
+		EmpManager e = new EmpManager();
+		DeptManager d = new DeptManager();
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("1. 사원관리  2. 부서관리  3. 종료");
+			int num = sc.nextInt();
+			sc.nextLine();
+			switch (num) {
+			case 1:
+				e.empManger();
+				break;
+			case 2:
+				d.deptManger();
+				break;
+			case 3:
+				System.exit(0);
+
+			}
+		}
+	}
+
+}
