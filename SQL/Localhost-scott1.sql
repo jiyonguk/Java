ac1977c82f54b1ba3c68167b9013ded717866a68
지용욱 <jyu7650@gmail.com>
2020-07-08 19:28:56 +0900

    20200708

---
+++ SQL/Localhost-scott1.sql
@@ -0,0 +1,22 @@
+--방명록 테이블
+
+create table guestbook_message (
+    message_id number(4) primary key,
+    quest_name varchar2(50) not null,
+    password varchar2(10) not null,
+    message varchar2(2000) not null
+);
+
+create sequence message_id_seq increment by 1 start with 1;
+
+
+insert into guestbook_message values (
+     message_id_seq.nextVal,
+     '테스터',
+     '1234',
+     '테스트중'
+);
+insert into guestbook_message values(message_id_seq.nextVal,'?','?','?');
+select * from guestbook_message;
+
+rollback;
