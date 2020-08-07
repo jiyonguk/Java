5ae2d47a12fb7bd4648d1d2c0c7a7035597e3a80
지용욱 <63032807+jiyonguk@users.noreply.github.com>
2020-08-07 18:27:06 +0900

    restFul

--- spring_project/APP-Member-Rest/src/main/java/com/aia/mm/service/MemberViewService.java
+++ spring_project/APP-Member-Rest/src/main/java/com/aia/mm/service/MemberViewService.java
@@ -5,11 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aia.mm.dao.MemberDao;
+import com.aia.mm.model.Member;
 
 @Service
 public class MemberViewService {
+	
 	private MemberDao dao;
 	
 	@Autowired
-	SqlSessionTemplate template;
+	private SqlSessionTemplate template;
+	
+	public Member getMember(int idx) {
+		
+		dao = template.getMapper(MemberDao.class);
+		
+		return dao.selectByIdx(idx);
+	}
+	
+	
+	
+	
+	
+	
+
 }
